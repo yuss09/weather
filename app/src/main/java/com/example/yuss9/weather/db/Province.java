a2e981cb3003c99ebb1490c4b1ec68392da1d619
yuss09 <yuss905@163.com>
2017-07-13 18:08:59 +0800

    加入了创建数据库和表的各项配置“

---
+++ app/src/main/java/com/example/yuss9/weather/db/Province.java
@@ -0,0 +1,39 @@
+package com.example.yuss9.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Aetos on 2017/7/13.
+ * Description:
+ **/
+
+public class Province extends DataSupport{
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
